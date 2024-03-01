d36d76580c2e894f5df6b89d9e28f95124d91b84
Vintesys | Aplicativos Personalizados <vintesys@gmail.com>
2024-03-01 20:27:29 -0300

    nova versão teste

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -293,8 +293,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "b4460a098d9b389629f2315daafe81ac
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fe0612d7592efc38f9a3d69bcd06509c",
-"/": "fe0612d7592efc38f9a3d69bcd06509c",
+"index.html": "7072b54db029ad508996cfad93e09a88",
+"/": "7072b54db029ad508996cfad93e09a88",
 "main.dart.js": "01bf3913d3e5a6289226a6a0bdad8c56",
 "manifest.json": "335936554e90495cc6cfcf541570e56f",
 "version.json": "84da346dca01c50249a809ce71128c0e"};
