208e7d8e50fe25a8499f9076a530162906b151d6
Vintesys | Aplicativos Personalizados <vintesys@gmail.com>
2024-03-03 12:56:56 -0300

    nova versão teste 17

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -294,9 +294,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "b4460a098d9b389629f2315daafe81ac
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f1a74b4484df1f5a368c7a3691acf264",
-"/": "f1a74b4484df1f5a368c7a3691acf264",
-"main.dart.js": "01bf3913d3e5a6289226a6a0bdad8c56",
+"index.html": "3d160b3c1ae49910b82bf3427a182144",
+"/": "3d160b3c1ae49910b82bf3427a182144",
+"main.dart.js": "9f467c5bb2712b4f401bc9992f4f59c0",
 "manifest.json": "335936554e90495cc6cfcf541570e56f",
 "version.json": "84da346dca01c50249a809ce71128c0e"};
 // The application shell files that are downloaded before a service worker can
